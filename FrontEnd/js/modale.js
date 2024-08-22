3dfd558b08c8bfbade9dd7f0e451c8b141273e6d
JuliaKardelice <julia.royer@live.fr>
2024-08-22 22:55:39 +0200

    correction du delete et amélioration de la previsualisation de l'image

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,5 +1,6 @@
 
 
+///variables globales
 const modal=document.getElementById("myModal");
 const btnModal=document.querySelector(".editMode");
 const overlay = document.getElementById("overlay");
@@ -9,9 +10,11 @@ const modalOne = document. querySelector('.modal-one');
 const modalTwo = document.querySelector('.modal-two');
 const btnBack = document.querySelector('.btn-back');
 const modalGallery=document.querySelector('titlemodal');
-
-
-
+const fileInput = document.getElementById('photo-input');
+const imgPreview = document.getElementById("imgPreview");
+const addPhoto = document.querySelector(".file-upload");
+const fileFormat=document.querySelector(".fileFormat")
+const iconPreview=document.getElementById("iconPreview");
 
   async function openModal() {
     modalOne.style.display = 'block';
@@ -51,21 +54,44 @@ const modalGallery=document.querySelector('titlemodal');
     figure.appendChild(img);
     figure.appendChild(btnDeletePhoto);
     galleryModal.appendChild(figure);
-    
-    function deleteWork() {
-      console.log("pour supprimer un travail");
-      figure.remove();
-      
-      }
+    let url = `http://localhost:5678/api/works/${work.id}`; ////supression par rapport à l'id
     // Attacher l'événement de suppression
     btnDeletePhoto.addEventListener('click', () => {
       console.log();
       
-      deleteWork();
+      deleteWork(url);
     });
   }
 
+const deleteWork = async (url)=>{
+const token=localStorage.getItem("token");
+if(!token){
+ alert("Vous n'êtes pas connecté") 
+return ////ARRET SCRIPT
+}
+const confirmation = confirm("êtes vous sûr de vouloir supprimer cette photo ?");
+if(!confirmation){
+return    ///////Si il ne veut pas supprimer
+}
+try {
+let response = await fetch(url,{
+method: "DELETE",
+headers: {
+"Authorization" :`Bearer ${token}`,
+"Accept":"application/json"
+}
+})
+if(!response.ok){
+throw new Error(`error http ${response.status}`)
+}
 
+} catch (error) {
+console.error(" Error while deleting",error) ////suprression n'a pas marché à cause telle erreure
+  
+}
+
+
+}
 
   
 function closeModal(){   
@@ -106,28 +132,28 @@ btnBack.addEventListener('click', () => {
 
 
 
-////MODALE TWO
-/*
-const btnUpload=document.querySelector(".file-upload");
-const validationNewWork=document.getElementById("validerButton")
+///gerer ajout de la photo ou le chargement de l'image
+fileInput.addEventListener("change",(event)=>{
+const file = event.target.files[0];
+const EXTENSTIONACCEPTED = ["jpg","png"];
+const fileName = file.name;
+const extension = fileName.split(".").pop().toLowerCase();   
 
-async function addNewWork() {
-const formData = new FormData();
-const btnUpload=document.getElementById("photo-input");
-const reponse= await fetch('http://localhost:5678/api/works',
-  {
-method:"POST",
-headers:{
-  'Content-Type:':'application/json',
+if(file && file.size < 4 * 1024 * 1024 && EXTENSTIONACCEPTED.includes(extension)){
+const reader = new FileReader() ///new 
+reader.onload = (e)=>{
+  imgPreview.src = e.target.result;
+  imgPreview.style.display="flex";
+  addPhoto.style.display="none";
+  iconPreview.style.display="none";
+  fileFormat.style.display="none";
 
-},
+  
+}
+reader.readAsDataURL(file)
+}else{
 
+alert("L'image n'a pas été chargée");
+}
 
-
-  }
-
-)
-
-};
-*/
-
+})
